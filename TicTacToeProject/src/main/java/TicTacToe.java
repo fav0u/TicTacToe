edcd3a867c9564ab4fd514757d6d14e9572cd422
phl <ic22b116@technikum-wien.at>
2023-12-30 12:53:25 +0100

    added checkEmptyField() method

--- TicTacToeProject/src/main/java/TicTacToe.java
+++ TicTacToeProject/src/main/java/TicTacToe.java
@@ -33,6 +33,29 @@ public class TicTacToe {
         } else currentPlayer = player1;
     }
 
+    public boolean checkEmptyField(int fieldNr) {
+        switch (fieldNr) {
+            case 1:
+                return board[0][0] == ' ';
+            case 2:
+                return board[0][1] == ' ';
+            case 3:
+                return board[0][2] == ' ';
+            case 4:
+                return board[1][0] == ' ';
+            case 5:
+                return board[1][1] == ' ';
+            case 6:
+                return board[1][2] == ' ';
+            case 7:
+                return board[2][0] == ' ';
+            case 8:
+                return board[2][1] == ' ';
+            case 9:
+                return board[2][2] == ' ';
+        }
+        return false;
+    }
 
     public void makeMove(int fieldNr) {
         switch (fieldNr) {
